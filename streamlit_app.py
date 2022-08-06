3a40a3b678fcb8071d5452b3d2aaf71aaafcafd7
Chanin Nantasenamat <51851491+dataprofessor@users.noreply.github.com>
2022-08-06 17:34:34 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,4 +2,9 @@ import streamlit as st
 
 st.title('✅ Checkbox App')
 
+if 'count' not in st.session_state:
+    st.session_state.count = 0
+
 func1_box = st.checkbox('Function 1')
+func2_box = st.checkbox('Function 2')
+func3_box = st.checkbox('Function 3')
